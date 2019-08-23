7e0930e618567f72e476bcf327856cd547605fd6
Luiz Cardozo <luizfcardozo@gmail.com>
2019-08-23 10:50:53 -0300

    desafio 4

---
+++ src/components/Techlist.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Header from './Header';
+
+class Techlist extends Component {
+  render() {
+    return <Header />;
+  }
+}
+
+export default Techlist;
